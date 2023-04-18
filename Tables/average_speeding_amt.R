3afd673095816697263429df4e5fda72a34d0fc6
Virginia Baskin <virginiab@campchampions.com>
2023-04-17 20:29:10 -0500

    not raw

--- Tables/average_speeding_amt.R
+++ Tables/average_speeding_amt.R
@@ -7,7 +7,7 @@ all_speed <- mutate(mini_htx, diff = speed - posted_speed) %>%
             median = median(diff, na.rm = TRUE), 
             sd = sd(diff, na.rm = TRUE))
 race_speed <- mutate(mini_htx, diff = speed - posted_speed) %>% 
-  filter(posted_speed >= 0) %>% group_by(raw_race) %>%
+  filter(posted_speed >= 0) %>% group_by(subject_race) %>%
   summarise(total = n(),  ## Function to use internally of summarise only
             mean = mean(diff, na.rm = TRUE),
             median = median(diff, na.rm = TRUE),
@@ -19,7 +19,7 @@ gender_speed <- mutate(mini_htx, diff = speed - posted_speed) %>%
             median = median(diff, na.rm = TRUE),
             sd = sd(diff, na.rm = TRUE)) 
 race_gender_speed <- mutate(mini_htx, diff = speed - posted_speed) %>% 
-  filter(posted_speed >= 0) %>% group_by(raw_race, subject_sex) %>%
+  filter(posted_speed >= 0) %>% group_by(subject_race, subject_sex) %>%
   summarise(total = n(),
             mean = mean(diff, na.rm = TRUE),
             median = median(diff, na.rm = TRUE),
